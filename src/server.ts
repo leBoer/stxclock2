7a17c3b1bdbb7d4d9e833e8b2ef4af3dfc8220e4
Nick Nisi <nick@nisi.org>
2017-05-14 09:04:12 +0400

    Tries to introduce caching

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,18 @@ import { renderModuleFactory } from '@angular/platform-server'
 import { enableProdMode } from '@angular/core'
 import { AppServerModuleNgFactory } from '../dist/ngfactory/src/app/app.server.module.ngfactory'
 import * as express from 'express';
+// import * as routeCache from 'route-cache';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
 import { ROUTES } from './routes';
-
 const PORT = process.env.PORT || 4000;
+
 
 enableProdMode();
 
 const app = express();
+const cache = require('route-cache');
 
 let template = readFileSync(join(__dirname, '..', 'dist', 'index.html')).toString();
 
@@ -29,26 +31,38 @@ app.set('views', 'src')
 
 app.use('/', express.static('dist', {index: false}));
 
-// routeCache
-var routeCache = require('route-cache');
-
-// cache route for 20 seconds
-app.get('/index', routeCache.cacheSeconds(20), function(req, res){
-  console.log('You will only see this every 20 seconds.');
-  res.send('this response will be cached');
-});
+// app.get('/', cache.cacheSeconds(10), function(req, res){
+//   // do your dirty work here...
+//   console.log('you will only see this every 10 seconds.');
+//   res.render('../dist/index', {
+//     req: req,
+//     res: res
+//   });
+//   return req.originalUrl;
+//   // res.send('this response will be cached');
+// });
 
 ROUTES.forEach(route => {
-  app.get(route, (req, res) => {
+  app.get(route, cache.cacheSeconds(10), function(req, res) {
     console.time(`GET: ${req.originalUrl}`);
     res.render('../dist/index', {
       req: req,
       res: res
     });
     console.timeEnd(`GET: ${req.originalUrl}`);
-    console.log('test');
   });
 });
+
+// ROUTES.forEach(route => {
+//   app.get(route, (req, res) => {
+//     console.time(`GET: ${req.originalUrl}`);
+//     res.render('../dist/index', {
+//       req: req,
+//       res: res
+//     });
+//     console.timeEnd(`GET: ${req.originalUrl}`);
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}!`);
