8ff6f733c18f990257596bebc9b68f9ab24569e3
Nick Nisi <nick@nisi.org>
2017-05-24 10:11:44 +0400

    Removes a console error message

--- src/app/exchange.service.ts
+++ src/app/exchange.service.ts
@@ -19,7 +19,6 @@ export class ExchangeService {
   }
 
   private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
   }
 
